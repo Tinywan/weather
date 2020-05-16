026633d3c0a16a67c8e4f811bf60536e0fe4b606
Tinywan <756684177@qq.com>
2020-05-16 17:36:14 +0800

    git commit -am "Add travis ci config file."

--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -2,6 +2,8 @@
 
 namespace Tinywan\Weather\Tests;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 use Tinywan\Weather\Exceptions\InvalidArgumentException;
 use Tinywan\Weather\Weather;
@@ -41,5 +43,31 @@ class WeatherTest extends TestCase
 
         // 如果没有抛出异常，就会运行到这行，标记当前测试没成功
         $this->fail('Failed to assert getWeather throw exception with invalid argument.');   
+    }
+
+    public function testGetWeather()
+    {
+        // 创建模拟接口响应值。
+        $response = new Response(200, [], '{"success": true}');
+
+        // 创建模拟 http client。
+        $client = \Mockery::mock(Client::class);
+
+        // 指定将会产生的行为（在后续的测试中将会按下面的参数来调用）。
+        $client->allows()->get('https://restapi.amap.com/v3/weather/weatherInfo', [
+            'query' => [
+                'key' => 'mock-key',
+                'city' => '深圳',
+                'output' => 'json',
+                'extensions' => 'base',
+            ]
+        ])->andReturn($response);  
+
+        // 将 `getHttpClient` 方法替换为上面创建的 http client 为返回值的模拟方法。
+        $w = \Mockery::mock(Weather::class, ['mock-key'])->makePartial();
+        $w->allows()->getHttpClient()->andReturn($client); // $client 为上面创建的模拟实例。
+
+        // 然后调用 `getWeather` 方法，并断言返回值为模拟的返回值。
+        $this->assertSame(['success' => true], $w->getWeather('深圳'));
     }
 }
